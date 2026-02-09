b7afa6b1fa127d28c4fb1c4ba8b51b35e50bde37
Aditya <rishkat1249@gmail.com>
2026-02-09 09:02:17 +0000

    SDL: drawing 2 rectanges for player 1 and 2 respectively

--- main.c
+++ main.c
@@ -2,30 +2,67 @@
 
 int main()
 {
-	SDL_Init(SDL_INIT_VIDEO);
+    // 1. Start SDL (graphics system)
+    SDL_Init(SDL_INIT_VIDEO);
 
-	SDL_Window *window = SDL_CreateWindow(
-		"My First SDL Window",
-		100, 100,
-		640, 480,
-		0
-	);
+    // 2. Create a window
+    SDL_Window *window = SDL_CreateWindow(
+        "Pong",
+        100, 100,
+        800, 600,
+        0
+    );
 
-	int running = 1;
+    // 3. Create a renderer (this draws pixels)
+    SDL_Renderer *renderer = SDL_CreateRenderer(
+        window,
+        -1,
+        SDL_RENDERER_ACCELERATED
+    );
 
-	SDL_Event event;
+    int running = 1;
+    SDL_Event event;
 
-	while (running){
-		while(SDL_PollEvent(&event)){
-			if (event.type ==SDL_QUIT){
-				running = 0;
-			}
-		}
-	}
+    // 4. Define Player 1 as a rectangle
+    SDL_Rect player1;
+    player1.x = 30;   // distance from left
+    player1.y = 250;  // distance from top
+    player1.w = 20;   // width
+    player1.h = 100;  // height
 
-	SDL_DestroyWindow(window);
+    SDL_Rect player2;
+    player2.x =750;
+    player2.y =250;
+    player2.w = 20;
+    player2.h = 100;
 
-	SDL_Quit();
+    // 5. Main loop
+    while (running) {
 
-	return 0;
+        // 6. Handle events (close button)
+        while (SDL_PollEvent(&event)) {
+            if (event.type == SDL_QUIT) {
+                running = 0;
+            }
+        }
+
+        // 7. Clear screen (background = black)
+        SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+        SDL_RenderClear(renderer);
+
+        // 8. Draw Player 1 (white rectangle)
+        SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+        SDL_RenderFillRect(renderer, &player1);
+        SDL_RenderFillRect(renderer, &player2);
+
+        // 9. Show everything on screen
+        SDL_RenderPresent(renderer);
+    }
+
+    // 10. Cleanup
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+
+    return 0;
 }
